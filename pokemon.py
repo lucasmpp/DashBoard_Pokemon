857dd21afe8266a55191874009c340158401793a
eddcorts <edfmcortes@gmail.com>
2021-05-20 05:59:48 -0300

    Tirando modo debug

--- pokemon.py
+++ pokemon.py
@@ -9,4 +9,4 @@ app = dash.Dash(external_stylesheets=[dbc.themes.SOLAR])
 app.layout = html.Div([filtros, conteudo(app)])
 
 if __name__ == '__main__':
-    app.run_server(port='8050', debug=True)
+    app.run_server(port='8050', debug=False)
